7e0a45ed2ea4deb6daaa1187791b74cb2de3fd7c
ovssim <ianjung2414@gmail.com>
2026-08-18 11:13:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let isSpinning = false;
 
 // ===================== ADMIN PASSWORD =====================
 let adminMode = false;
-const ADMIN_PASSWORD = "layla";
+const ADMIN_PASSWORD = "royal";
 
 
 // ===================== INIT =====================
